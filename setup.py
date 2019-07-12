831b95268f91f8781ec1195838262fc2194faaaa
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 13:56:38 +0100

    Update versioning system

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ import re
 with open('README.md', 'r') as fd:
     long_description = fd.read()
 
+import os, re;
+with open(os.path.join(os.path.dirname(__file__), 'pythonvideoannotator_module_backgroundfinder','__init__.py')) as fd:
+	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
 setup(
 	name='Python video annotator - module - background finder',
-	version="0.4",
+	version=version,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
